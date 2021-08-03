72d88f3467f3c8db51cf28d0027dde7c71fb33e0
Ria333 <geraria333@gmail.com>
2021-08-03 14:25:14 +0300

    in progress//stuck on a 6 task

---
+++ post-details.js
@@ -0,0 +1,8 @@
+let JSONPost = new URL(location)
+console.log(location);
+//     .searchParams.get('post');
+//
+// let post = JSON.parse(JSONPost);
+//
+// let divBig3 = document.getElementsByClassName('massive3')[0]
+// divBig3.innerHTML =`${post.id} ${post.title} ${post.body}`
---
+++ user-details.js
@@ -0,0 +1,45 @@
+let JSONUser = new URL(location).searchParams.get('user');
+let user = JSON.parse (JSONUser);
+
+let divBig = document.getElementsByClassName('massive2')[0]
+divBig.innerHTML =`${user.id} ${user.name} ${user.username} ${user.email} ${user.address.street} ${user.address.suite} ${user.address.city} ${user.address.zipcode} ${user.address.geo.lat} ${user.address.geo.lng} ${user.phone} ${user.website} ${user.company.name}  ${user.company.catchPhrase} ${user.company.bs}`;
+
+
+let postUser = document.createElement ('button');
+postUser.innerText = 'post of current user';
+divBig.appendChild(postUser);
+postUser.onclick = function (){
+
+    fetch (`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
+        .then (function (value){ return value.json();})
+        .then (function (posts){
+
+            let PostsBox = document.getElementsByClassName('PostsBox')[0]
+
+            for (const post of posts) {
+
+                let Posts = document.createElement('p');
+                Posts.innerText = post.title;
+
+                let postDetal = document.createElement ('button');
+
+                let getDetailsAboutPost = document.createElement ('a');
+                getDetailsAboutPost.innerText = 'Get Details About Post';
+                getDetailsAboutPost.href = `user.id=${JSON.stringify(post)}`
+
+                PostsBox.appendChild(Posts);
+                postDetal.appendChild(getDetailsAboutPost);
+                PostsBox.appendChild(postDetal);
+
+            }})}
+
+divBig.appendChild(postUser);
+
+
+
+
+
+
+
+// 5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
